b2661634a3905b54856ecea339493bfc60add6bc
Lorde Aleister <lordealeister@gmail.com>
2020-06-01 12:03:51 -0300

    Fix CWE ID 489 error

--- src/functions-dev.php
+++ src/functions-dev.php
@@ -3,11 +3,7 @@
 if (!function_exists('dd')) {
 	function dd(...$args)
 	{
-		if (function_exists('dump')) {
-			dump(...$args);
-		} else {
-			var_dump(...$args);
-		}
+		var_dump(...$args);
 		die;
 	}
 }
